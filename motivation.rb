6b263c57c68ff2560085d037f3fd2f3690cd6de2
“JulianBoralli” <jboralli@icloud.com>
2016-09-21 01:36:19 -0700

    Refactor horizontal line

--- motivation.rb
+++ motivation.rb
@@ -5,22 +5,22 @@ while true
 	input = gets.chomp.downcase
 	case input
 	when "yes"
-		60.times { print "-"}; puts ""
+		puts "-"*60
 		puts "No worries, here is a powerful quote that will cheer you up:"
 		puts Faker::ChuckNorris.fact
-		60.times { print "-"}; puts ""
+		puts "-"*60
 	when "no"
-		60.times { print "-"}; puts ""
+		puts "-"*60
 		puts "I am glad you feel good, now stop playing around and go back to coding!"
-		60.times { print "-"}; puts ""
+		puts "-"*60
 		break
 	else
-		60.times { print "-"}; puts ""
+		puts "-"*60
 		puts "Your situation is worst than I thought, you can't even type yes or no!"
 		puts "You've been coding for too many hours my friend, but worry not ..."
 		puts "Here is a VERY POWERFUL quote to bring you back to life:"
 		puts Faker::ChuckNorris.fact
-		60.times { print "-"}; puts ""
+		puts "-"*60
 	end
 
 end
